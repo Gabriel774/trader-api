3b30725d4274ed5f608461867f040e152104b74d
gabriel <gabrielsantossousa774@gmail.com>
2023-08-11 19:01:37 -0300

    fix: cors ajustado

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
     credentials: true,
-  });
+  };
+
+  app.enableCors(options);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(3333);
